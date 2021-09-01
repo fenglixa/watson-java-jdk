3a8cea30977c53bc808cce4632644f36d569d05f
fenglixa <fenglixa@cn.ibm.com>
2021-09-01 20:57:43 +0800

    update v2 centralbot API

--- assistant/src/test/java/com/ibm/watson/assistant/v2/AssistantCentralBot.java
+++ assistant/src/test/java/com/ibm/watson/assistant/v2/AssistantCentralBot.java
@@ -11,8 +11,7 @@ public class AssistantCentralBot {
         String assistantId = "35913f3e-e863-4ef7-ab46-c6d62aab6580";
         String textMessage = "what's my credit";
 
-        MessageContext context = new MessageContext.Builder().build();
-        Assistant service = new Assistant("2019-02-28", authenticator);
+        Assistant service = new Assistant("2021-06-14", authenticator);
         service.setServiceUrl("https://api.eu-gb.assistant.watson.cloud.ibm.com/instances/" + instansID);
 
         // get session ID
@@ -20,13 +19,37 @@ public class AssistantCentralBot {
         SessionResponse sessionResponse = service.createSession(createSessionOptions).execute().getResult();
         String sessionId = sessionResponse.getSessionId();
 
-        MessageInput input = new MessageInput.Builder().text(textMessage).build();
-        MessageOptions messageOptions = new MessageOptions.Builder().assistantId(assistantId).sessionId(sessionId)
+        MessageInputOptions inputOptions = new MessageInputOptions.Builder().returnContext(true).build();
+
+        MessageInput input = new MessageInput.Builder().messageType("text").text(textMessage).options(inputOptions).build();
+        MessageOptions messageOptions = new MessageOptions.Builder(assistantId, sessionId)
                 .input(input).build();
         MessageResponse messageResponse = service.message(messageOptions).execute().getResult();
+        MessageContext context = new MessageContext.Builder().build();
         context = messageResponse.getContext();
 
-        System.out.println(messageResponse);
-        System.out.println(context);
+        // get child assistant id
+        String childAssistantId = (String) context.skills().get("main skill").userDefined().get("assistant_id");
+        System.out.println("DEBUG: Child assistant id is: " + childAssistantId);
+
+        // get the final result
+        CreateSessionOptions childCreateSessionOptions = new CreateSessionOptions.Builder().assistantId(childAssistantId).build();
+        SessionResponse childSessionResponse = service.createSession(childCreateSessionOptions).execute().getResult();
+        String childSessionId = childSessionResponse.getSessionId();
+
+        MessageOptions childOptions = new MessageOptions.Builder(childAssistantId, childSessionId)
+        .input(input).build();
+        MessageResponse childResponse = service.message(childOptions).execute().getResult();
+        System.out.println("Response message: " + childResponse.getOutput().getGeneric().get(0).text());
+
+        // delete the sessions
+        DeleteSessionOptions childDeleteOptions = new DeleteSessionOptions.Builder(childAssistantId, childSessionId).build();
+        service.deleteSession(childDeleteOptions).execute();
+
+        DeleteSessionOptions options = new DeleteSessionOptions.Builder(assistantId, sessionId).build();
+        service.deleteSession(options).execute();
+
+        // System.out.println(messageResponse);
+        // System.out.println(context);
     }
 }
